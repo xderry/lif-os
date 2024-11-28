da2e04187dea2b50eb89a7632ba8c2fd836e7cae
Derry Shribman <xxderry@gmail.com>
2024-11-29 01:30:14 +0200

    update

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-import handler from 'serve-handler';
+import serve from 'serve-handler';
 import http from 'http';
+import process from 'process';
 
 const server = http.createServer((request, response)=>{
+  const opt = {directoryListing: false, cleanUrls: false};
+  const url = request.url;
+  let file;
+  const is_prefix = prefix=>{
+    if (url.startsWith(prefix))
+      return {prefix: prefix, rest: url.substr(prefix.length)};
+  };
+  let v;
+  if (v=is_prefix('/.lif/'));
+  else if (v=is_prefix('/.lif/pkgroot/'))
+    file = v.rest;
+  else
+    file = '/public'+url;
+  if (file)
+    opt.rewrites = [{source: '**', destination: file}];
+  console.log(`req ${url} -> ${file}`);
   // You pass two more arguments for config and middleware
   // More details here: https://github.com/vercel/serve-handler#options
-  return handler(request, response);
+  return serve(request, response, opt);
 });
 
-server.listen(3000, ()=>{
-  console.log('Running at http://localhost:3000');
+let port = 3000;
+let [...argv] = [...process.argv];
+argv.shift();
+argv.shift();
+while (argv[0]!=undefined){
+  if (argv[0]=='-p'){
+    argv.shift();
+    port = +argv.shift();
+  }
+  else
+    break;
+}
+if (argv[0]!=undefined)
+  throw 'invalid args '+JSON.stringify(argv);
+
+server.listen(port, ()=>{
+  console.log(`Running at http://localhost:${port}`);
 });
