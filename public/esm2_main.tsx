665b33387805a7131d9c9252c07fc8e74da3288b
Derry Shribman <xxderry@gmail.com>
2024-12-25 03:54:56 +0200

    update

---
+++ public/esm2_main.tsx
@@ -0,0 +1,20 @@
+// from code.esm.sh
+//import React from "react";
+//import {createRoot} from "react-dom";
+import App from "./esmsh_app.tsx";
+
+let global = self;
+
+let define = function(){
+};
+define.amd = {};
+
+global.define = define;
+
+let i = await import("https://unpkg.com/react@18/umd/react.development.js");
+console.log('import amd', i);
+let r = await import("https://esm.sh/react@18.2.0");
+console.log('import esm', r);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
