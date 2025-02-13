43ff506c1e25c9245b3b93c028634aaabe8f0ba7
Derry Shribman <xxderry@gmail.com>
2025-02-13 19:57:55 +0200

    update

--- lif_kernel_sw.js
+++
@@ -1,2 +0,0 @@
-/*global importScripts*/
-importScripts('https://unpkg.com/lif-kernel@0.1.2/kernel.js');
---
+++ public/lif_kernel_sw.js
@@ -0,0 +1,2 @@
+/*global importScripts*/
+importScripts('https://unpkg.com/lif-kernel@0.1.4/kernel.js');
