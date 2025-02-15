1d779225c70ee6d14df71833fcdecd7bd240da1b
Derry Shribman <xxderry@gmail.com>
2025-02-15 21:31:19 +0200

    update

--- contexts/menu/index.tsx
+++ contexts/menu/index.tsx
@@ -1,7 +1,7 @@
 import Menu from "components/system/Menu";
 import contextFactory from "contexts/contextFactory";
 import useMenuContextState from "contexts/menu/useMenuContextState";
-
 const { Provider, useContext } = contextFactory(useMenuContextState, <Menu />);
-
+import {set_useMenu} from "components/system/Menu";
+set_useMenu(useContext);
 export { Provider as MenuProvider, useContext as useMenu };
