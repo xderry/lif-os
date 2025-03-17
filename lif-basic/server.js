87aeed354d3bfcfee1d450685113600a8a2b138e
Derry Shribman <xxderry@gmail.com>
2025-03-18 01:43:30 +0200

    update

--- lif-basic/server.js
+++ lif-basic/server.js
@@ -3,7 +3,8 @@ import server from '../lif-kernel/server.js';
 let map = {
   '.': './index.html',
   '/lif_kernel_sw.js': './',
-  '/lif-kernel': '../lif-kernel/',
+  '/style.css': './',
   '/lif-basic': './',
+  '/lif-kernel': '../lif-kernel/',
 };
-server({map});server({map});
+server({map});
