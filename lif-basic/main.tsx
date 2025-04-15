254e47793da187670267edc47277de2451e30956
Derry Shribman <xxderry@gmail.com>
2025-04-15 07:15:54 +0300

    update

--- lif-basic/main.tsx
+++ lif-basic/main.tsx
@@ -3,6 +3,12 @@ import React from "react";
 import {createRoot} from "react-dom";
 import App from "./app.tsx";
 
+// set favicon
+let link = document.createElement('link');
+link.rel = 'icon';
+link.href = '/.lif/npm/lif-kernel/favicon.ico';
+document.head.appendChild(link);
+// start app
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
 
