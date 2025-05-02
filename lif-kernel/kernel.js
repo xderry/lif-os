d0c56b5894e7da24dbc06e129d1095b3151a4461
Derry Shribman <xxderry@gmail.com>
2025-05-02 22:51:26 +0300

    update

--- lif-kernel/kernel.js
+++ lif-kernel/kernel.js
@@ -765,7 +765,9 @@ let pkg_export_lookup = (pkg, file)=>{
       }
       return;
     }
-    return check_val(res, v.module, 'mjs') ||
+    // TODO: do we need need to "detect" cjs/mjs/amd at this stage?
+    return check_val(res, v.browser, 'cjs') ||
+      check_val(res, v.module, 'mjs') ||
       check_val(res, v.import, 'mjs') ||
       check_val(res, v.default, 'cjs') ||
       check_val(res, v.require, 'amd');
@@ -793,8 +795,8 @@ let pkg_export_lookup = (pkg, file)=>{
     if (v = parse_section(exports))
       return v;
     if (file=='.'){
-      return check_val([], pkg.module, 'mjs') ||
-        check_val([], pkg.browser, 'cjs') ||
+      return check_val([], pkg.browser, 'cjs') ||
+        check_val([], pkg.module, 'mjs') ||
         check_val([], pkg.main, 'cjs') ||
         check_val([], 'index.js', 'cjs');
     }
