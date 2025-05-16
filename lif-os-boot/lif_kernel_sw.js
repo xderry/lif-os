af42b30420f9cd758753d2da08dc78a5a1157036
Derry Shribman <xxderry@gmail.com>
2025-05-16 05:55:16 +0300

    update

--- lif-os-boot/lif_kernel_sw.js
+++ lif-os-boot/lif_kernel_sw.js
@@ -1,4 +1,5 @@
 /*global importScripts*/
-importScripts('https://unpkg.com/lif-kernel@1.1.0/kernel.js');
-//globalThis.lif_kernel_local = true;
-//importScripts('/lif-kernel/kernel.js');
+//importScripts('https://unpkg.com/lif-kernel@1.1.2/kernel.js');
+let u = new URLSearchParams(location.search);
+let base = u.get('lif_kernel_base') || '/lif-kernel/';
+importScripts(base+'kernel.js');
