a243f696f14a2604ae7b3fb3cb87894bce02fafc
Derry Shribman <xxderry@gmail.com>
2025-05-18 06:38:47 +0300

    update

--- lif-basic/server.js
+++ lif-basic/server.js
@@ -1,7 +1,3 @@
 #!/usr/bin/env node
 import server from 'lif-kernel/server.js';
-let map = {
-  '/lif-basic': './',
-  '/lif-kernel': '../lif-kernel/', // for local kernel development
-};
-server({map, root: import.meta.dirname});
+server({root: import.meta.dirname});
