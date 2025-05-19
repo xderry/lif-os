aa7d78f5a7300ece9abff84f8820bc5ba549b6f5
Derry Shribman <xxderry@gmail.com>
2025-05-19 03:35:23 +0300

    update

--- lif-os-boot/server.js
+++ lif-os-boot/server.js
@@ -1,8 +1,3 @@
 #!/usr/bin/env node
-import server from 'lif-kernel/server.js';
-let map = {
-  '/lif-os-boot': './',
-  '/lif-os': '../',
-  '/lif-kernel': '../lif-kernel/', // for local development
-};
-server({map, root: import.meta.dirname});
+import server from '../lif-kernel/server.js';
+server({map: {'/lif-os': '../'}, root: import.meta.dirname});
