cb596940e319b5c7a49271aee2861aab462aeb00
Derry Shribman <xxderry@gmail.com>
2025-08-25 22:32:02 +0300

    update

--- lif-kernel/kernel.js
+++ lif-kernel/kernel.js
@@ -477,16 +477,17 @@ let lpm_imp_lookup = ({lpm_pkg, imp})=>{
     return ret_err('invalid lpm uri import');
   if (u.ver || u.reg=='local')
     return imp;
-  let _imp = lpm_imp_ver_lookup(lpm_pkg, imp, true, true);
+  let _imp = lpm_imp_ver_lookup({lpm_pkg, imp, peer: true, dev: true});
   if (!_imp || _imp.startsWith(':peer:')){
     if (lpm_pkg_app &&
-      (_imp = lpm_imp_ver_lookup({lmod: mod_self, pkg: lpm_pkg_app.pkg}, imp,
-      true, true)))
+      (_imp = lpm_imp_ver_lookup({
+        lpm_pkg: {lmod: mod_self, pkg: lpm_pkg_app.pkg}, imp,
+        peer: true, dev: true})))
     {
       return _imp;
     }
     for (let l = lpm_pkg_app; l; l = l.parent){
-      if (!(_imp = lpm_imp_ver_lookup(l, imp, true, true)))
+      if (!(_imp = lpm_imp_ver_lookup({lpm_pkg: l, imp, peer: true, dev: true})))
         continue;
       return _imp;
     }
@@ -566,7 +567,7 @@ const mjs_import_mjs = (export_default, path, q)=>{
   return js;
 };
 
-let lpm_imp_ver_lookup = (lpm_pkg, imp, peer, dev)=>{
+let lpm_imp_ver_lookup = ({lpm_pkg, imp, peer, dev})=>{
   let pkg = lpm_pkg.pkg;
   let lmod = T_lpm_lmod(imp);
   let npm = T_lpm_to_npm(lmod);
@@ -1198,7 +1199,8 @@ function test_kernel(){
     react_p: '^18.3.1',
     dom_p: '>=18.3.1',
   }}};
-  t = (imp, v)=>assert_eq(v, lpm_imp_ver_lookup(lpm_pkg, imp, true, true));
+  t = (imp, v)=>assert_eq(v, lpm_imp_ver_lookup({lpm_pkg, imp,
+    peer: true, dev: true}));
   t('npm/pages/_app.tsx', 'npm/lif-os/pages/_app.tsx');
   t('npm/loc/file.js', 'local/loc//file.js');
   t('npm/react', 'npm/react@18.3.1');
