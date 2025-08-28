e7e6611e1aa41a00f8ea22a898504bdbd64225a0
Derry Shribman <xxderry@gmail.com>
2025-08-28 21:27:20 +0300

    update

--- lif-kernel/util.js
+++ lif-kernel/util.js
@@ -450,7 +450,8 @@ let match_glob_to_regex = exports.match_glob_to_regex =
 let match_glob = exports.match_glob =
   (glob, value)=>match_glob_to_regex(glob).test(value);
 let qs_enc = exports.qs_enc = (q, qmark)=>{
-  let _q = (''+new URLSearchParams(q)).replaceAll('%2F', '/');
+  let _q = (''+new URLSearchParams(q))
+  .replaceAll('%2F', '/').replaceAll('%40', '@').replaceAll('%3A', ':');
   return _q ? (qmark ? '?' : '')+_q : '';
 };
 let qs_append = exports.qs_append = (url, q)=>{
