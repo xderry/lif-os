951fbc45dab8f11bf7870ae88953f350b6d250c8
Derry Shribman <xxderry@gmail.com>
2025-10-15 00:00:44 +0300

    update

--- lif-basic/server.js
+++ lif-basic/server.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 import server from 'lif-kernel/server_lib.js';
-server({map: {'/lif-basic': import.meta.dirname}});
+server({map: {'/lif-basic': '.', '/index.html': './index.html'},
+  root: import.meta.dirname});
