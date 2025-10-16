5537c15f691a50c1df3fd525224afb75a36e0ecc
Derry Shribman <xxderry@gmail.com>
2025-10-16 05:53:41 +0300

    update

--- lif-kernel/server.js
+++ lif-kernel/server.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 import server from './server_lib.js';
-server({map: {'/lif-kernel': import.meta.dirname}});
+let cwd = import.meta.dirname;
+let map = {}
+map['/lif-kernel'] = cwd;
+// local dev
+map['/lif-basic'] = cwd+'../lif-basic';
+map['/lif-os-boot'] = cwd+'../lif-os-boot';
+map['/lif-os'] = cwd+'../';
+map['/lif-coin'] = cwd+'../../lif-coin';
+server({map});
